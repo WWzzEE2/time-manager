3b4dc52f9971163eb3303fb8e3002123c795bd78
theamusing <2455505305@qq.com>
2022-10-30 03:11:51 +0800

    calendar page added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,26 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplication.backstage.*
 import com.example.myapplication.ui.theme.MyApplicationTheme
-import com.example.myapplication.front.*
 
 class MainActivity : ComponentActivity() {
-    lateinit var schedule:Schedule
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        var config = TestDataConfig(30,30,15,12)
-
-        schedule = Schedule(this, config)
         setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
@@ -41,7 +33,9 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting(name: String) {
-    BottomNavigation()
+
+    CalendarPage()
+    //Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
@@ -49,5 +43,6 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     MyApplicationTheme {
         Greeting("Android")
+
     }
 }
