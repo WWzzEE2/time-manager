783e7e9e95ca8f545e1f756b94607997e3fd0d97
theamusing <2455505305@qq.com>
2022-10-30 03:11:51 +0800

    calendar page added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,9 +30,12 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+
+    CalendarPage()
+    //Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
@@ -39,5 +43,6 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     MyApplicationTheme {
         Greeting("Android")
+
     }
 }
