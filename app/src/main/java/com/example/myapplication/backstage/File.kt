f1e23789b8fbde15cac5f245dc66ab085870ed60
Nodemrax <warco8450@gmail.com>
2022-10-30 20:53:54 +0800

    Pre Version 2: File IO

--- app/src/main/java/com/example/myapplication/backstage/File.kt
+++ app/src/main/java/com/example/myapplication/backstage/File.kt
@@ -7,45 +7,32 @@ import com.google.gson.reflect.TypeToken
 import java.io.FileInputStream
 import java.io.FileOutputStream
 
-internal fun save(schedule: Schedule, context: Context) {
-    saveAsJSON(schedule.getAllDDl(), context.openFileOutput("ddl.json", Context.MODE_PRIVATE))
-    saveAsJSON(schedule.getAllCourse(), context.openFileOutput("course.json", Context.MODE_PRIVATE))
+fun save(ddlList: List<DDlInfo>, courseList: List<CourseInfo>, activity: ComponentActivity) {
+    saveAsJSON(ddlList, activity.openFileOutput("ddl.json", Context.MODE_PRIVATE))
+    saveAsJSON(courseList, activity.openFileOutput("course.json", Context.MODE_PRIVATE))
 
 }
 
-internal fun load(schedule: Schedule, context: Context)  {
-    val ddlList = loadAndParse<DDlInfo>(context.openFileInput("ddl.json"))
-    val courseList = loadAndParse<CourseInfo>(context.openFileInput("course.json"))
-
-    for (ddl in ddlList)
-        schedule.addDDl(ddl)
-
-    for (course in courseList)
-        schedule.addCourse(course)
+fun load(activity: ComponentActivity) : Pair<List<DDlInfo>, List<CourseInfo>> {
+    val ddlList = loadAndParse<DDlInfo>(activity.openFileInput("ddl.json"))
+    val courseList = loadAndParse<CourseInfo>(activity.openFileInput("course.json"))
 
     for (course in courseList) {
         for (template in course.TimeInfo) {
             template.info = course
         }
     }
-
-    // TODO load it
-    schedule.termStartTime = 0
+    return Pair(ddlList, courseList)
 }
 
 private fun <T> saveAsJSON(list:List<T>, file: FileOutputStream) = file.use{it.writer().use{ writer -> writer.write(Gson().toJson(list))}}
 
 private fun <T> loadAndParse(file: FileInputStream): List<T> {
     var jsonData: String
-    try {
-        file.use {
-            it.reader().use { reader ->
-                jsonData = reader.readText()
-            }
+    file.use {
+        it.reader().use {
+            reader -> jsonData = reader.readText()
         }
-    }
-    catch (e: NoSuchFileException) {
-        return emptyList()
     }
     return Gson().fromJson(jsonData, object : TypeToken<List<T>>() {}.type)
 }
