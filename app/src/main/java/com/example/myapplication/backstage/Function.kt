53612770907dd79c6c2ba4a87a9bc8fb65a0bda9
晓尧 黄 <804947737@qq.com>
2022-10-31 21:26:56 +0800

    Couse.kt 1.给CourseInfo加入了一个函数 addCourse(courseTime:CourseTemplate),将对应CourseTemplate实例加入TimeInfo中。 2.引入全局数值RowStart、RowEnd两个数组(上限为24),记录每一行的开始和结束位于一天中的第几分钟 Function.kt 1.加入函数getMin(timeStamp: Long),返回一个时间戳位于一天中的第几分钟 2.加入函数getRow(timeStamp: Long),返回一个时间戳对应于第几行，(若无匹配行则返回-1)

--- app/src/main/java/com/example/myapplication/backstage/Function.kt
+++ app/src/main/java/com/example/myapplication/backstage/Function.kt
@@ -35,3 +35,23 @@ fun getHour(timeStamp: Long) = Calendar.getInstance().let {
     it.time = Date(timeStamp)
     (it[Calendar.HOUR_OF_DAY].toShort())
 }
+
+fun getMin(timeStamp: Long): Long {
+    //输入时间戳，返回位于一天中的第几分钟
+    val start_day=(timeStamp/24/3600/1000)*24*3600*1000
+    val past_min=(timeStamp-start_day)/1000/60
+    return past_min
+}
+
+fun getRow(timeStamp: Long):Short{
+    //输入时间戳，返回属于哪一列
+    val min=getMin(timeStamp)
+    var column=-1
+    for(index in 1..RowStart.size)
+        if(min>= RowStart.get(index)!!&&min< RowEnd.get(index)!!)
+        {
+            column=index
+            break
+        }
+    return column.toShort()
+}
