03e12b1cb9a16872f35c5eca22a973979325dc4b
Nodemrax <warco8450@gmail.com>
2022-10-31 19:39:23 +0800

    Bug fix: Data Generation

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.backstage.TestDataConfig
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        TestDataConfig(1,2,3,4)
         setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
