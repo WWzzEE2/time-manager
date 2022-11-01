dc67767579306b944391ebb2fbc0bfb3b78c62f4
kang <2027962673@qq.com>
2022-11-01 16:21:47 +0800

    添加课程和编辑课程页面

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,22 +1,27 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplication.backstage.TestDataConfig
+import com.example.myapplication.backstage.*
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
+    lateinit var schedule:Schedule
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        TestDataConfig(1,2,3,4)
+        var config = TestDataConfig(20,1,1,12)
+        schedule = Schedule(this,config)
         setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
@@ -31,9 +36,12 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    EditPage()
+    //CalendarPage()
+    //Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
@@ -41,5 +49,6 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     MyApplicationTheme {
         Greeting("Android")
+
     }
 }
