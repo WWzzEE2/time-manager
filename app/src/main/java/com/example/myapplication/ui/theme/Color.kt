234ae92c5e0c6752c0ec38e4999ddd8e8a9ecccf
theamusing <2455505305@qq.com>
2022-11-08 16:18:25 +0800

    add ddl display in calendar page add color selector

--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import kotlin.random.Random
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -9,3 +10,22 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Red_T = Color(0x9EFF4D4D)
+
+class courseBlockColor {
+    companion object Obj {
+        private val colors: List<Color> =
+            mutableListOf(
+                Color(0xFF77B4BF),
+                Color(0xFF97CED7),
+                Color(0xFF46909D),
+                Color(0xFFB1D8DF),
+            )
+
+        fun getColor(): Color {
+            return colors.random();
+        }
+    }
+
+}
