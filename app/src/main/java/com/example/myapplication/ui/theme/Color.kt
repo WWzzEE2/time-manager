5abf03f3cf0f8b52fb118092490ade3cb32f1320
theamusing <2455505305@qq.com>
2022-11-09 01:30:40 +0800

    add setting page

--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -11,7 +11,7 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val Red_T = Color(0x9EFF4D4D)
+val Red_T = Color(0x70FF1F1F)
 
 class courseBlockColor {
     companion object Obj {
