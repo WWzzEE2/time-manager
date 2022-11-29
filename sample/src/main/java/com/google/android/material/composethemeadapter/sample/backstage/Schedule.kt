7a630a179c9e533c62608a28638bf9e37e2839b6
晓尧 黄 <804947737@qq.com>
2022-11-29 11:00:13 +0800

    Function.kt 加入getWeekStamp(startingTime: Long, week: Long): Long函数 和getTime(startingTime: Long, week: Long, day: Long): Long 函数 返回某周第一天零点对应的时间戳和某周的某一天零点对应的时间戳
    Schedule.kt
    加入对每节课开始和结束时间的初始化，该时间的引用方式为
    schedule.termInfo.rowStart[i]
    schedule.termInfo.rowEnd[i]
    返回值为课程开始时间在一天中的分钟数
    加入schedule.getWeekStamp(week: Long):Long 函数
    和getTime(week: Long, day: Long): Long 函数
    是Function中函数的不用显式输入开始时间的使用

--- sample/src/main/java/com/google/android/material/composethemeadapter/sample/backstage/Schedule.kt
+++ sample/src/main/java/com/google/android/material/composethemeadapter/sample/backstage/Schedule.kt
@@ -139,9 +139,34 @@ class Schedule(private val context: Context, testData: TestDataConfig? = null) {
         internal set
 
     init {
-
         if (testData != null) {
             generateTestData(testData)
+            termInfo.rowStart.add(getPastMin(8,0))
+            termInfo.rowStart.add(getPastMin(9,0))
+            termInfo.rowStart.add(getPastMin(10,10))
+            termInfo.rowStart.add(getPastMin(11,10))
+            termInfo.rowStart.add(getPastMin(13,0))
+            termInfo.rowStart.add(getPastMin(14,0))
+            termInfo.rowStart.add(getPastMin(15,10))
+            termInfo.rowStart.add(getPastMin(16,10))
+            termInfo.rowStart.add(getPastMin(17,0))
+            termInfo.rowStart.add(getPastMin(18,40))
+            termInfo.rowStart.add(getPastMin(19,40))
+            termInfo.rowStart.add(getPastMin(20,40))
+
+            termInfo.rowEnd.add(getPastMin(8,50))
+            termInfo.rowEnd.add(getPastMin(9,50))
+            termInfo.rowEnd.add(getPastMin(11,0))
+            termInfo.rowEnd.add(getPastMin(12,0))
+            termInfo.rowEnd.add(getPastMin(13,50))
+            termInfo.rowEnd.add(getPastMin(14,50))
+            termInfo.rowEnd.add(getPastMin(16,0))
+            termInfo.rowEnd.add(getPastMin(17,0))
+            termInfo.rowEnd.add(getPastMin(18,0))
+            termInfo.rowEnd.add(getPastMin(19,30))
+            termInfo.rowEnd.add(getPastMin(20,30))
+            termInfo.rowEnd.add(getPastMin(21,50))
+
 //            val weekSec = 1000L * 3600 * 24 * 7
 //            val course = CourseInfo("Test Course", termInfo.startingTime, termInfo.startingTime + weekSec * testData.totWeek, ArrayList(), "test", "Void")
 //            course.timeInfo.add(CourseTemplate(0, 0 , 2, 1))
@@ -184,6 +209,16 @@ class Schedule(private val context: Context, testData: TestDataConfig? = null) {
      * @see com.example.myapplication.backstage.getWeek
      */
     fun getWeek(time: Long) = getWeek(termInfo.startingTime, time)
+
+    /**
+     * @see com.example.myapplication.backstage.getWeekStamp
+     */
+    fun getWeekStamp(week: Long) = getWeekStamp(termInfo.startingTime,week)
+
+    /**
+     * @see com.example.myapplication.backstage.getTime
+     */
+    fun getTime(week: Long, day: Long)=getTime(termInfo.startingTime,week,day)
 
     /**
      * Save all data to disk
