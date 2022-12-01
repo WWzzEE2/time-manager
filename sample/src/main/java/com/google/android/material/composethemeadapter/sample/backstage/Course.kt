d127c25a267de087cf0bb5b9c826de9cf77333df
kang <2027962673@qq.com>
2022-12-01 20:44:29 +0800

    新增了DDL添加的功能，暂不支持编辑已有DDL

--- sample/src/main/java/com/google/android/material/composethemeadapter/sample/backstage/Course.kt
+++ sample/src/main/java/com/google/android/material/composethemeadapter/sample/backstage/Course.kt
@@ -37,11 +37,11 @@ class TermInfo internal constructor(
 }
 
 data class DDlInfo(
-    val name: String,                  //DDL名字
+    var name: String,                  //DDL名字
     val id: Long,
-    val endingTime: Long,              //DDL结束时间
+    var endingTime: Long,              //DDL结束时间
     val prompt: String,                //DDL描述
-    val startingTime: Long             //DDL开始工作时间
+    var startingTime: Long             //DDL开始工作时间
 ) {
     fun wrapTime(time: Long): String = if (time < 10) "0$time" else time.toString()
     fun getString(termInfo: TermInfo): String {
