660f13e54386a6c62f3a3a636dbaab8f73a73327
c-x-b <954642703@qq.com>
2022-12-06 00:11:45 +0800

    完成教学网的SSO登录，完善错误处理

--- sample/src/main/java/com/google/android/material/composethemeadapter/sample/backstage/Import.kt
+++ sample/src/main/java/com/google/android/material/composethemeadapter/sample/backstage/Import.kt
@@ -3,40 +3,70 @@ package com.example.myapplication.backstage
 import android.content.Context
 import android.util.Log
 import androidx.compose.runtime.mutableStateListOf
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.google.android.material.composethemeadapter.sample.MainActivity
 import com.google.android.material.composethemeadapter.sample.backstage.CourseInfo
 import com.google.android.material.composethemeadapter.sample.backstage.CourseTemplate
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import okhttp3.*
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import java.io.IOException
+import kotlin.random.Random
 
 
 data class UserAccount(
     // future to use
     var UserName: String,
     var ID: String,
-    var PassWord: String,
+    var PassWord: String
     // Login Status to add
-)
+) {
+    var token = ""
+}
+
+class storeCookieJar:CookieJar {
+    private var cookieStore: HashMap<String?, List<Cookie>> = HashMap()
+    override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
+        cookieStore[url.host] = cookies
+    }
+
+    override fun loadForRequest(url: HttpUrl): List<Cookie> {
+        val cookies = cookieStore[url.host]
+        return cookies ?: ArrayList()
+    }
+}
 
 class Import {
 
-    private var defaultHeaders =  Headers.Builder()
-    .add("Accept","application/json, text/javascript, */*; q=0.01")
-    .add("Accept-Encoding", "gzip, deflate, br")
-    .add("Accept-Language", "en-US,en;q=0.9")
-    .add("Host", "iaaa.pku.edu.cn")
-    .add("Origin","https://iaaa.pku.edu.cn")
-    .add("Connection","keep-alive").build()
+    private val iaaaHeaders =  Headers.Builder()
+        .add("Accept","application/json, text/javascript, */*; q=0.01")
+        .add("Accept-Encoding", "gzip, deflate, br")
+        .add("Accept-Language", "en-US,en;q=0.9")
+        .add("Host", "iaaa.pku.edu.cn")
+        .add("Origin","https://iaaa.pku.edu.cn")
+        .add("Connection","keep-alive").build()
+
+    private val courseHeader = Headers.Builder()
+        .add("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+        //.add("Accept-Encoding","gzip, deflate, br")
+        .add("Accept-Language", "en-US,en;q=0.9")
+        .add("Host","course.pku.edu.cn")
+        .add("Upgrade-Insecure-Requests","1")
+        .add("Connection","keep-alive")
+        .add("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36")
+        .add("Refer","https://iaaa.pku.edu.cn/")
+        .build()
 
     private val homeUrl="https://iaaa.pku.edu.cn/iaaa/oauth.jsp"
     private val loginUrl="https://iaaa.pku.edu.cn/iaaa/oauthlogin.do"
-    private val redirUrl="https://course.pku.edu.cn/webapps/bb-sso-bb_bb60/execute/authValidate/campusLogin"
+    private val ssoUrl="https://course.pku.edu.cn/webapps/bb-sso-bb_bb60/execute/authValidate/campusLogin"
+
+    private val cookieJar = storeCookieJar()
 
     //DEBUG:your account here
-    private val user = UserAccount("","1","")
+    private val user = UserAccount("username","1","password")
     //DEBUG:your account here
 
     /**
@@ -124,9 +154,6 @@ class Import {
             course.endingTime=endingTime
             course.addTemplate(templateList)
 
-
-
-
             for (s in templateList)
                 Log.d("TestdataTemplateList", s.toString())
 
@@ -141,15 +168,55 @@ class Import {
         return true
     }
 
-    private fun visitIAAA() {
-        val homeHeaders = Headers.Builder().addAll(defaultHeaders)
+    private fun setToken(jsonData:String):Boolean {
+        val mapper = jacksonObjectMapper()
+        try {
+            val jsonNode = mapper.readTree(jsonData)
+            val state = jsonNode.get("success")
+            if (state.toString()=="false")
+                return false
+            val tokenNode = jsonNode.get("token")
+            user.token = tokenNode.textValue()
+            Log.d("TestWebLogin", "token get: ${user.token}")
+        } catch (e:Exception) {
+            Log.d("TestWebToken","ERR, jsonData: $jsonData")
+            e.printStackTrace()
+            return false
+        }
+        return true
+    }
+
+    private fun visitCourse():Boolean {
+        val courseUrl = "https://course.pku.edu.cn"
+        val okHttpClient = OkHttpClient.Builder().cookieJar(cookieJar).build()
+        val request = Request.Builder()
+            .headers(courseHeader).url(courseUrl).get().build()
+        val call = okHttpClient.newCall(request)
+        try {
+            var response = call.execute()
+            if (response.isSuccessful) {
+                Log.d("TestWebCourse", response.body!!.string())
+            }
+            else {
+                Log.d("TestWebCourse", "code: ${response.code}")
+                return false
+            }
+        } catch (e:IOException) {
+            e.printStackTrace()
+            return false
+        }
+        return true
+    }
+
+    private fun visitIAAA():Boolean {
+        val homeHeaders = Headers.Builder().addAll(iaaaHeaders)
             .add("Referer","https://course.pku.edu.cn/")
             .add("Upgrade-Insecure-Requests","1").build()
         val formBody = FormBody.Builder()
             .add("appID", "blackboard")
             .add("appName","1")
-            .add("redirectUrl",redirUrl).build()
-        val okHttpClient = OkHttpClient()
+            .add("redirectUrl",ssoUrl).build()
+        val okHttpClient = OkHttpClient.Builder().cookieJar(cookieJar).build()
         val request = Request.Builder()
             .headers(homeHeaders).url(homeUrl).post(formBody).build()
         val call = okHttpClient.newCall(request)
@@ -157,15 +224,21 @@ class Import {
         try {
             var response = call.execute()
             if (response.isSuccessful) {
-                Log.d("TestWebLogin", response.body!!.string())
+                Log.d("TestWebIAAA", response.body!!.string())
+            }
+            else {
+                Log.d("TestWebIAAA", "code: ${response.code}")
+                return false
             }
         } catch (e:IOException) {
             e.printStackTrace()
+            return false
         }
+        return true
     }
 
-    private fun loginIAAA() {
-        val loginHeaders = Headers.Builder().addAll(defaultHeaders)
+    private fun loginIAAA():Boolean {
+        val loginHeaders = Headers.Builder().addAll(iaaaHeaders)
             .add("Referer", homeUrl)
             .add("X-Requested-With","XMLHttpRequest").build()
         val formBody=FormBody.Builder()
@@ -175,8 +248,8 @@ class Import {
             .add("randCode","")
             .add("smsCode","")
             .add("otpCode","")
-            .add("redirUrl",redirUrl).build()
-        val okHttpClient = OkHttpClient()
+            .add("redirUrl",ssoUrl).build()
+        val okHttpClient = OkHttpClient.Builder().cookieJar(cookieJar).build()
         val request = Request.Builder()
             .headers(loginHeaders).url(loginUrl).post(formBody).build()
         val call = okHttpClient.newCall(request)
@@ -184,17 +257,75 @@ class Import {
         try {
             var response = call.execute()
             if (response.isSuccessful) {
-                Log.d("TestWebLogin", response.body!!.string())
+                val jsonData = response.body!!.string()
+                Log.d("TestWebLogin", jsonData)
+                if (!setToken(jsonData)) {
+                    Log.d("TestWebLogin", "ERROR: cannot get token, data:" + jsonData)
+                    return false
+                }
+            }
+            else {
+                Log.d("TestWebLogin", "code: ${response.code}")
+                return false
             }
         } catch (e:IOException) {
             e.printStackTrace()
+            return false
         }
+        return true
+    }
+
+    private fun loginSSO():Boolean {
+        if (user.token.isEmpty())
+            return false
+        val getUrl = ssoUrl.toHttpUrlOrNull()!!.newBuilder()
+        getUrl.addQueryParameter("_rand", Random.nextFloat().toString())
+        getUrl.addQueryParameter("token",user.token)
+        Log.d("TestWebSSO","usr: ${getUrl.build()}")
+        val okHttpClient = OkHttpClient.Builder().cookieJar(cookieJar).build()
+        val request = Request.Builder()
+            .headers(courseHeader).url(getUrl.build()).get().build()
+        val call = okHttpClient.newCall(request)
+        try {
+            var response = call.execute()
+            if (response.isSuccessful) {
+                Log.d("TestWebSSO", response.body!!.string())
+            }
+            else {
+                Log.d("TestWebSSO", "code: ${response.code}")
+                return false
+            }
+        } catch (e:IOException) {
+            e.printStackTrace()
+            return false
+        }
+        return true
     }
 
     fun importFromCourse() {
-        GlobalScope.launch {
-            visitIAAA()
-            loginIAAA()
+        val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+            Log.d("TestWebERR", "catch a coroutine error: $throwable")
+        }
+        GlobalScope.launch(exceptionHandler) {
+            // visit web to get cookie
+            if (!visitCourse()) {
+                Log.d("TestWebERR","visit course failed")
+                return@launch
+            }
+            // IAAALogin
+            if (!visitIAAA()) {
+                Log.d("TestWebERR", "visit IAAA failed")
+                return@launch
+            }
+            if (!loginIAAA()) {
+                Log.d("TestWebERR","IAAA login failed")
+                return@launch
+            }
+            // SSOLogin + visit main page
+            if (!loginSSO()) {
+                Log.d("TestWebERR","SSO login failed")
+                return@launch
+            }
         }
     }
 
